30b49afa44af789420caee0fdaf20c64e09dd9c9
ValooFX <mail@heijulien.de>
2016-02-08 02:42:12 +0100

    Brain Eye script

---
+++ Funghi/Assets/Scripts/BrainEyeMover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrainEyeMover : MonoBehaviour
+{
+
+    public Sprite[] eyeSprites;
+    SpriteRenderer sr;
+
+    public float RandomTimebetweenMin = 0.2f;
+    public float RandomTimebetweenMax = 2f;
+	// Use this for initialization
+	void Start ()
+	{
+	    sr = GetComponent<SpriteRenderer>();
+        if (!sr) return;
+	    Invoke("ChangeEye", 1f);
+	}
+
+    void ChangeEye()
+    {
+        sr.sprite = eyeSprites[Random.Range(0, eyeSprites.Length)];
+        Invoke("ChangeEye", Random.Range(RandomTimebetweenMin, RandomTimebetweenMax));
+    }
+}
