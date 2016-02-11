6ecb5ef43de0fb3378b89d965dfbdbe807c0c742
ValooFX <mail@heijulien.de>
2016-02-11 20:52:28 +0100

    Music

---
+++ Funghi/Assets/Scripts/Helper/AudioFadeIn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioFadeIn : MonoBehaviour
+{
+
+    float _maxVolume;
+    AudioSource aus;
+    public float FadeInSpeed = 0.5f;
+	// Use this for initialization
+	void Start ()
+	{
+	    aus = GetComponent<AudioSource>();
+        if (!aus) return;
+	    _maxVolume = aus.volume;
+	    aus.volume = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (aus.volume >= _maxVolume) Destroy(this);
+	    aus.volume += Time.deltaTime*FadeInSpeed;
+	}
+}
